115822357f7a640fa1c1887f829a23709961412b
Ethan Ng <wlx5502@outlook.com>
2021-12-19 15:36:40 +0800

    feat: add wrapper to preprocess input text before summerization

--- cn_summary/utils.py
+++ cn_summary/utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def get_src_path(path):
     prefix = os.environ.setdefault(
@@ -6,3 +7,11 @@ def get_src_path(path):
     ) 
     return os.path.abspath(os.path.join(prefix, path))
 
+def text_legal(func):
+    def wrapper(text):
+        text = re.sub('\s', '', text)
+        seperators = ['。', '!', '！', '？', '?']
+        if text[-1] not in seperators:
+            text += '。'
+        return func(text)
+    return wrapper
