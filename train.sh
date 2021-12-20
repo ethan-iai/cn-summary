53a8b1b324050df651cf1e9d4e379939eacf1edc
Ethan Ng <wlx5502@outlook.com>
2021-12-20 12:16:04 +0800

    feat: train script for bert-seq2seq training

---
+++ train.sh
@@ -0,0 +1,5 @@
+python -m cn_summary.engine.bert_seq2seq_summarizer.train ./data \
+                                                    --epochs 90 \
+                                                    --batch-size 32 \
+                                                    --gpu 1
+
